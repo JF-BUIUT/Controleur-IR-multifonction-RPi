d9ae2f757f8718bd7b502a34a557f8e0465893c5
Jonael FELICIEN <jonael.felicien@etu.univ-guyane.fr>
2025-12-18 14:20:33 -0300

    Update installation script for Raspberry Pi

--- requirement.sh
+++ requirement.sh
@@ -3,24 +3,17 @@ echo "====================================================="
 echo "     INSTALLATION AUTOMATIQUE DES BIBLIOTHÈQUES"
 echo "====================================================="
 
-echo "[1/3] Mise à jour du Raspberry Pi..."
+echo "[1/2] Mise à jour du Raspberry Pi..."
 sudo apt autoremove
 sudo apt update -y && sudo apt upgrade -y
 
-echo "[2/3] Installation des paquets..."
+echo "[2/2] Installation des paquets..."
 sudo apt install python3-full 
 sudo apt install python3.13-venv
+sudo apt install pipx
 sudo apt install python3-pip 
 sudo apt install python3-pigpio 
 sudo apt install python3-bleak
 sudo apt install python3-pillow
 sudo apt install python3-pygame
-sudo apt install pipx
-
-echo "[3/3] Création de l'environnement python..."
-python3 -m venv setup 
-
-echo "L'environnement python à bien été crée !"
-
-echo "Lancer le avec '   source setup/bin/activate     ' "
 
